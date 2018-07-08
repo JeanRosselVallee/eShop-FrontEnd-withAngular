bf8f8935e1fe1dc2fdfbe938b413d2fd300d53e8
jv <jv.isa4031@gmail.com>
2018-07-08 19:10:17 +0200

    added component work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AdminProductComponent } from './admin-product/admin-product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { UserService }      from './service/user.service';
 import { UserComponent } from './user/user.component';
+import { WorkInProgressComponent } from './work-in-progress/work-in-progress.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { UserComponent } from './user/user.component';
     AdminProductComponent,
     ProductDetailComponent,
     UserComponent,
+    WorkInProgressComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,7 +51,7 @@ import { UserComponent } from './user/user.component';
           pathMatch: 'full'
         },
         {
-          path: "catalog/:id",
+          path: "detail/:id",
           component: ProductDetailComponent,
           pathMatch: 'full'
         },
@@ -59,17 +61,77 @@ import { UserComponent } from './user/user.component';
           pathMatch: 'full'
         },
         {
-          path: "admin/catalog",
-          component: AdminProductComponent,
-          pathMatch: 'full'
-        },
-        {
           path: "login",
           component: UserComponent,
           pathMatch: 'full'
         },
         {
+          path: "cart",
+          component: CartComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "subscribe",
+          component: UserComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "admin/catalog",
+          component: AdminProductComponent,
+          pathMatch: 'full'
+        },
+        {
           path: "admin/users",
+          component: UserComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "users/remove",
+          component: UserComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "users/update",
+          component: UserComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "carts",
+          component: CartComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "carts/remove",
+          component: CartComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "carts/add-product",
+          component: CartComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "carts/remove-product",
+          component: CartComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "products/add",
+          component: CatalogComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "products/remove",
+          component: CatalogComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "products/update",
+          component: CatalogComponent,
+          pathMatch: 'full'
+        },
+        {
+          path: "me",
           component: UserComponent,
           pathMatch: 'full'
         }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,22 @@
 
 export const environment = {
   production: false,
-  //getCatalogURL:  "http://localhost:8080/products",
-  getCatalogURL:  "http://192.168.134.113:8080/products",
+  getCatalogURL:  "http://localhost:8080/products",
+  //getCatalogURL:  "http://192.168.134.113:8080/products",
   postCatalogURL: "http://localhost:8080/products/add",
   delCatalogURL:  "http://localhost:8080/products/rem",
+  //putCatalogURL:  "http://localhost:8080/products/update",
   postCartURL:    "http://localhost:8080/carts/add-product",
   getCartURL:     "http://localhost:8080/carts/get-cart-by-id",
   getCartIdURL:   "http://localhost:8080/carts/get-cart-id",
   delCartURL:     "http://localhost:8080/carts/rem",
+  //delCartProdURL: "http://localhost:8080/carts/remove-product",
+  //getAllCartsURL: "http://localhost:8080/carts/all",
   authUrl:        "http://localhost:8080/users/auth", 
   getUsersUrl:    "http://localhost:8080/users", 
   postUserUrl:    "http://localhost:8080/users/add", 
   delUserUrl:     "http://localhost:8080/users/remove", 
+  //putUserUrl:     "http://localhost:8080/users/update",
   loginUrl:       "http://localhost:8080/users/login"
 };
 
